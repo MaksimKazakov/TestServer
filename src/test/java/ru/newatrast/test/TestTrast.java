46933ef9f0e9d23e098e62b309bc9865b1f9aba9
Maxim Kazakov <kazakovms@a-trast.ru>
2024-09-17 15:22:04 +0500

    fix - test5

--- src/test/java/ru/newatrast/test/TestTrast.java
+++ src/test/java/ru/newatrast/test/TestTrast.java
@@ -1,73 +1,70 @@
 package ru.newatrast.test;
 
-import com.codeborne.selenide.Configuration;
-import io.qameta.allure.Step;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.apache.logging.log4j.util.PropertySource;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFPictureData;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
-import java.util.Map;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestTrast {
-
-//    private static final Map<String, Object> selenoidOptions = Map.of(
-//            "enableVNC", true,
-//            "enableLog", true,
-//            "enableVideo", true
-//    );
-private static final Map<String, Object> selenoidOptions = Map.of(
-        "enableVNC", true,
-        "enableLog", true,
-        "enableVideo", true,
-         "headless", true // Убираем временно для теста
-);
-
-
-    static {
-        // Установка конфигурации Selenide для подключения к Selenoid
-        Configuration.remote = "http://147.45.153.130:4444/wd/hub";
-        Configuration.browser = "chrome";
-        Configuration.browserSize = "1920x1080";
-
-        // Установка дополнительных selenoid options для Selenide
-       // Configuration.browserCapabilities.setCapability("selenoid:options", selenoidOptions);
-    }
-
-    private int getResult() {
-        return 3;
-    }
-
     @Test
-    @DisplayName("Open main page new.a-trast.ru")
-    @Step("Open page https://new.a-trast.ru")
-    public void test() {
+    public void test() throws IOException {
         open("https://new.a-trast.ru");
-        sleep(15000);
-    }
+        $(By.xpath(".//div[contains(text(),'Вход')]")).click();
+        $(By.xpath("//div[contains(@class,'login-form')]")).$(By.name("USER_LOGIN")).setValue("bayduganova");
+        $(By.xpath("//div[contains(@class,'login-form')]")).$(By.name("USER_PASSWORD")).setValue("test_b2b");
+        $(By.xpath(".//button/div[text()='Войти']")).click();
+        $x(".//a[@href='/user/returns']").click();
 
-    @Test
-    @DisplayName("Secondary test")
-    @Step("Test 2")
-    void firstTest() {
-        int result = getResult();
-        System.out.println("###      firstTest()");
-        Assertions.assertTrue(result > 2);
-    }
+        // Клик по элементу для скачивания
+        $("span[data-tooltip='Скачать акт']").click();
 
-    @Test
-    @DisplayName("Test false")
-    @Step("Test 3")
-    void secondTest() {
-        int result = getResult();
-        System.out.println("###      secondTest()");
-        Assertions.assertTrue(result > 2);
-    }
+        // Делаем паузу для ожидания открытия новой вкладки
+        sleep(10000); // Увеличьте время, если это необходимо
 
-    @AfterEach
-    public void tearDown() {
-        closeWebDriver(); // Закрытие браузера после выполнения теста
-    }
-}
+
+        // Находим последний загруженный файл в папке build/downloads
+        Path downloadsDir = Paths.get("build/downloads");
+        Optional<Path> latestFile;
+
+        try (Stream<Path> paths = Files.walk(downloadsDir)) {
+            latestFile = paths
+                    .filter(Files::isRegularFile)
+                    .filter(path -> path.toString().endsWith(".docx"))
+                    .max(Comparator.comparing(Path::getFileName));
+
+        }
+
+        if (latestFile.isEmpty()) {
+            throw new IOException("Файл не был найден в папке загрузок.");
+        }
+
+        // Проверяем наличие изображений в скачанном файле
+        try (FileInputStream fis = new FileInputStream(latestFile.get().toFile());
+             XWPFDocument document = new XWPFDocument(fis)) {
+
+            boolean hasImage = document.getAllPictures().size() > 0;
+
+            if (hasImage) {
+                System.out.println("Документ содержит изображение.");
+            } else {
+                System.out.println("В документе изображений нет.");
+            }
+        }
+    }}
