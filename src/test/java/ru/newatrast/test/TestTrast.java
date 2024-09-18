c0a19a8e97eb871742644c74e3a1d898578098d4
Maxim Kazakov <kazakovms@a-trast.ru>
2024-09-18 17:43:09 +0500

    fix - test18

--- src/test/java/ru/newatrast/test/TestTrast.java
+++ src/test/java/ru/newatrast/test/TestTrast.java
@@ -1,10 +1,9 @@
 package ru.newatrast.test;
+
 import com.codeborne.selenide.Configuration;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -12,71 +11,50 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestTrast {
-    static {
-        // Настройки ChromeOptions
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setExperimentalOption("prefs", new HashMap<String, Object>() {{
-            put("download.default_directory", "/home/selenium/Downloads");
-            put("download.prompt_for_download", false);
-            put("download.directory_upgrade", true);
-            put("safebrowsing.enabled", true);
-        }});
-        chromeOptions.addArguments("--disable-gpu");
-        chromeOptions.addArguments("--headless");
-
-        // Установка конфигурации Selenide для подключения к Selenoid
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-
-        Configuration.remote = "http://147.45.153.130:4444/wd/hub";
-        Configuration.browser = "chrome";
-        Configuration.browserSize = "1920x1080";
-        Configuration.browserCapabilities = capabilities;
-    }
-//
-   private final String downloadFilepath = "/home/selenium/Downloads";
 
     @Test
     public void test() throws IOException {
+        // Устанавливаем папку для загрузок
+        String downloadFilepath = "/root/downloads";
+        Configuration.downloadsFolder = downloadFilepath;
+
+        // Открытие сайта
         open("https://new.a-trast.ru");
         $(By.xpath(".//div[contains(text(),'Вход')]")).click();
         $(By.xpath("//div[contains(@class,'login-form')]")).$(By.name("USER_LOGIN")).setValue("bayduganova");
         $(By.xpath("//div[contains(@class,'login-form')]")).$(By.name("USER_PASSWORD")).setValue("test_b2b");
         $(By.xpath(".//button/div[text()='Войти']")).click();
         $x(".//a[@href='/user/returns']").click();
+
+        // Клик по элементу для скачивания
         $("span[data-tooltip='Скачать акт']").click();
 
-        // Ожидание загрузки
-        sleep(60000); // Увеличьте время, если это необходимо
+        // Ожидание загрузки файла
+        Path downloadedFile = $(By.cssSelector("span[data-tooltip='Скачать акт']"))
+                .download().toPath();  // Этот метод дождётся завершения загрузки
 
-        // Проверка файла
-        Path downloadsPath = Paths.get(downloadFilepath);
-        Optional<Path> latestFile;
-
-        try (Stream<Path> paths = Files.walk(downloadsPath)) {
-            latestFile = paths
-                    .filter(Files::isRegularFile)
-                    .filter(path -> path.toString().endsWith(".docx"))
-                    .max(Comparator.comparing(Path::getFileName));
-        }
-
-        if (latestFile.isEmpty()) {
+        // Проверка, что файл скачан
+        if (downloadedFile == null || !Files.exists(downloadedFile)) {
             throw new IOException("Файл не был найден в папке загрузок.");
         }
 
         // Проверка наличия изображений в скачанном файле
-        try (FileInputStream fis = new FileInputStream(latestFile.get().toFile());
+        try (FileInputStream fis = new FileInputStream(downloadedFile.toFile());
              XWPFDocument document = new XWPFDocument(fis)) {
+
             boolean hasImage = document.getAllPictures().size() > 0;
 
-            System.out.println(hasImage ? "Документ содержит изображение." : "В документе изображений нет.");
+            if (hasImage) {
+                System.out.println("Документ содержит изображение.");
+            } else {
+                System.out.println("В документе изображений нет.");
+            }
         }
     }
 }
